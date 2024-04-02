0231818479bcc3bbab34420211e0b5e63c918ec9
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-04-02 08:09:35 +0530

    Create FIRST FOLLOW.py

---
+++ FIRST FOLLOW.py
@@ -0,0 +1,50 @@
+def first(grammar, term):
+    first_set = set()
+    if term not in grammar:
+        return set(term)
+    for prod in grammar[term]:
+        first_sym = prod[0]
+        if first_sym not in grammar:
+            first_set.add(first_sym)
+        else:
+            first_set |= first(grammar, first_sym)
+    return first_set
+
+def follow(grammar, term, checked=None):
+    if checked is None:
+        checked = set()
+    if term in checked:
+        return set()
+    checked.add(term)
+    follow_set = set()
+    if term == list(grammar.keys())[0]:
+        follow_set.add('$')
+    for key, productions in grammar.items():
+        for prod in productions:
+            if term in prod:
+                next_sym_pos = prod.index(term) + 1
+                if next_sym_pos < len(prod):
+                    next_sym = prod[next_sym_pos]
+                    follow_set |= first(grammar, next_sym)
+                else:
+                    follow_set |= follow(grammar, key, checked)
+    return follow_set
+
+# Example usage:
+# grammar = {
+#     'E': ['TG'],
+#     'G': ['+TG', 'ε'],
+#     'T': ['FS'],
+#     'S': ['*FS', 'ε'],
+#     'F': ['(E)', 'i']
+# }
+
+grammar = {
+    'E': ['AB'],
+    'A': ['ilove'],
+    'B': ['jtptutorials']
+}
+
+for non_terminal in grammar:
+    print(f'FIRST({non_terminal}) = {first(grammar, non_terminal)}')
+    print(f'FOLLOW({non_terminal}) = {follow(grammar, non_terminal)}')
